90436028c829420e703ae22d14fb878e2094563b
lexing2008 <lexing2008@yandex.ru>
2021-08-12 16:43:53 +0100

    add HierarhyHelper

---
+++ common/helpers/HierarchyHelper.php
@@ -0,0 +1,170 @@
+<?php
+namespace common\helpers;
+
+use Yii;
+
+/**
+ * Хэлпер HierarchyHelper
+ * позволяет работать с иерархическим списком
+ */
+abstract class HierarchyHelper 
+{
+    /**
+     * ID главного родителя в иерархии
+     */
+    const MAIN_PARENT_ID = 0;
+    
+    /**
+     * Имя директории кэша
+     */
+    const CACHE_DIR_NAME = '';
+    
+    /**
+     * Имя файла кэша
+     */
+    const CACHE_FILE_NAME = '';
+    
+    /**
+     * Категории
+     * @var array 
+     */
+    public $category;
+    
+    /**
+     * Упорядоченные по дереву элементы
+     * @var array
+     */
+    public $items;
+    
+    /**
+     * Количество элементов в $items. Вычисляется как счетчик. используется как счетчик
+     * @var int
+     */
+    protected $currentCountItems = 0;
+         
+    /**
+     * Конструктор класса
+     * @param bool $autoLoad автоматическая загрука из кэша, если не получилось из кэша, то из БД при создании объекта
+     */
+    public function __construct(bool $autoLoad = true) 
+    {
+        // подгружаем всю информацию
+        if ($autoLoad){
+            // если не удалось подгрузить из кэша
+            if(!$this->loadFromCache()){
+                // подгружаем из таблицы
+                $this->loadFromTable();
+            }
+        }
+    }
+
+    /**
+     * Подгружает из кэша данные иерархии
+     * @return bool получилось подгрузить из кэша данные иерархии
+     */
+    public function loadFromCache(): bool
+    {
+        $this->items = $this->getItemsFromCache();
+        
+        return !empty($this->items);
+    }
+
+    /**
+     * Получение элементов иерархического списка из таблицы
+     */
+    abstract function getItemsFromTable(): array;
+    
+    /**
+     * Получение элементов иерархического списка из кэша
+     */
+    abstract function getItemsFromCache(): array; 
+    
+    /**
+     * Сохранение элементов иерархического списка в кэш
+     */
+    abstract function saveItemsToCache();
+        
+    /**
+     * Загрузка информации из БД и формирование правильной иерархической структуры
+     */
+    public function loadFromTable() 
+    {
+        // Получение элементов иерархического списка из таблицы
+        $records = $this->getItemsFromTable();
+        $rows = count($records);
+        unset($this->category);
+        unset($this->items);
+        $this->category = [];
+        for ($i = 0; $i < $rows; ++$i) {
+            $this->category[$records[$i]['id']] = $records[$i];
+        }
+
+        // устанавливаем текущее количество в нуль
+        $this->currentCountItems = 0;
+        // приводим в структурированный вид иерархический список
+        $this->nextItem( self::MAIN_PARENT_ID ); // (Родитель) pid = 0; (Уровень вложенности) level = 0; Начинаем отсчет уровня вложенности	
+        // сохраняем в кэш
+        $this->saveItemsToCache();
+    }
+    
+    /**
+     * Функция находит все элементы родителя
+     * @param int $pid идентификатор родителя. Для самого верхнего это 0
+     * @param int $level уровень вложенности
+     */
+    private function nextItem(int $pid, int $level = 0) 
+    {
+        // просматриваем весь массив
+        foreach ($this->category as $key => $val) {
+            // элемент пренадлежит родителю
+            if ($val['pid'] == $pid) {
+                // добавляем текущий элемент в наш массив упорядоченных элементов
+                $this->items[$this->currentCountItems]          = $val;
+                $this->items[$this->currentCountItems]['level'] = $level;
+                // удаляем текущий элемент из массива
+                unset($this->category[$key]);
+                // увеличивае счетчик
+                $this->currentCountItems++;
+                // рекурсивно ищем потомков для данного
+                $this->nextItem($key, $level + 1);
+            }
+        }
+    }
+    
+    /** 
+     * Возвращает массив потомков заданного элемента
+     * @param int $parent  идентификатор родителя. Приведен к int.
+     * @return array массив потомков
+     */
+    public function getChildrens(int $parent) 
+    {
+        if($parent == self::MAIN_PARENT_ID){
+            return $this->items;
+        }
+        
+        $arr        = [];
+        $flagLevel  = 0;
+        $size       = count($this->items); 
+        for ($i = 0; $i < $size; ++$i) {
+            if ($this->items[$i]['id'] == $parent) {
+                $flagLevel = $this->items[$i]['level'];
+                ++$i;
+                while ($i < count($this->items) && $this->items[$i]['level'] > $flagLevel) {
+                    $arr[] = $this->items[$i];
+                    ++$i;
+                }
+                return $arr;
+            }
+        }
+        return $arr;
+    }
+    
+    /**
+     * Возвращает путь к файлу кэша
+     * @return string путь к файлу кэша
+     */
+    protected function getCachePath(): string
+    {
+        return Yii::getAlias('@root/' . static::CACHE_DIR_NAME . static::CACHE_FILE_NAME);
+    }
+}
--- common/helpers/RubricsHelper.php
+++ common/helpers/RubricsHelper.php
@@ -1,131 +1,59 @@
 <?php
 namespace common\helpers;
 
-use Yii;
 use common\models\db\Rubrics;
-
 
 /**
  * Хэлпер RubricsHelper
  * позволяет работать с иерархическим списком рубрик
  */
-class RubricsHelper 
+class RubricsHelper extends HierarchyHelper
 {
     /**
-     * ID главного родителя в иерархии
+     * Имя директории кэша
      */
-    const MAIN_PARENT_ID = 0;
+    const CACHE_DIR_NAME = 'cache/rubrics/';
     
     /**
-     * Категориии  расположенные как в БД
-     * @var array 
+     * Имя файла кэша
      */
-    public $original;
+    const CACHE_FILE_NAME = 'rubrics.dat';
     
     /**
-     * Категории
-     * @var array 
+     * Получение элементов иерархического списка из кэша
+     * @return array элементы иерархического списка
      */
-    public $category;
-    
-    /**
-     * Упорядоченные по дереву элементы
-     * @var array
-     */
-    public $items;
-    
-    /**
-     * Количество элементов в $items. Вычисляется как счетчик. используется как счетчик
-     * @var int
-     */
-    protected $currentCountItems = 0;
-         
-    /**
-     * Конструктор класса
-     * @param bool $autoLoadFromTable автоматическая загрука из БД при создании объекта
-     */
-    public function __construct(bool $autoLoadFromTable = true) 
-    {
-        // подгружаем всю информацию
-        if ($autoLoadFromTable){
-            $this->loadFromTable();
-        }
-    }
-
-    /**
-     * Загрузка информации из БД и формирование правильной иерархической структуры
-     */
-    public function loadFromTable() {
-        
-        $records = Rubrics::find()->asArray()
-                                  ->all();
-        $rows = count($records);
-        unset($this->category);
-        unset($this->original);
-        unset($this->items);
-        $this->category = [];
-        for ($i = 0; $i < $rows; $i++) {
-            $this->category[$records[$i]['id']] = $records[$i];
-        }
-
-        $this->original = $this->category;
-        // устанавливаем текущее количество в нуль
-        $this->currentCountItems = 0;
-        $this->nextItem( self::MAIN_PARENT_ID ); // (Родитель) pid = 0; (Уровень вложенности) level = 0; Начинаем отсчет уровня вложенности	
-    }
-    
-    
-    /**
-     * Функция находит все элементы родителя
-     * @param int $pid идентификатор родителя. Для самого верхнего это 0
-     * @param int $level уровень вложенности
-     */
-    private function nextItem(int $pid, int $level = 0) 
-    {
-        if(empty($this->category)){
-            return;
-        }
-        // просматриваем весь массив
-        foreach ($this->category as $key => $val) {
-            // элемент пренадлежит родителю
-            if ($val['pid'] == $pid) {
-                // добавляем текущий элемент в наш массив упорядоченных элементов
-                $this->items[$this->currentCountItems]          = $val;
-                $this->items[$this->currentCountItems]['level'] = $level;
-                // удаляем текущий элемент из массива
-                unset($this->category[$key]);
-                // увеличивае счетчик
-                $this->currentCountItems++;
-                // рекурсивно ищем потомков для данного
-                $this->nextItem($key, $level + 1);
-            }
-        }
-    }
-    
-    /** 
-     * Возвращает массив потомков заданного элемента
-     * @param int $parent  идентификатор родителя. Приведен к int.
-     * @return array массив потомков
-     */
-    public function getChildrens(int $parent) 
-    {
-        if($parent == self::MAIN_PARENT_ID){
-            return $this->items;
+    public function getItemsFromCache(): array 
+    { 
+        $items = [];
+        // путь к файлу кэша
+        $path = $this->getCachePath();
+        // проверяем существование файла
+        if(file_exists($path)){
+            // получаем элементы иерархического списка
+            $items = json_decode(file_get_contents($path), true);
         }
         
-        $arr        = [];
-        $flagLevel  = 0;
-        for ($i = 0; $i < count($this->items); $i++) {
-            if ($this->items[$i]['id'] == $parent) {
-                $flagLevel = $this->items[$i]['level'];
-                ++$i;
-                while ($i < count($this->items) && $this->items[$i]['level'] > $flagLevel) {
-                    $arr[] = $this->items[$i];
-                    ++$i;
-                }
-                return $arr;
-            }
-        }
-        return $arr;
+        return $items; 
+    }
+    
+    /**
+     * Получение элементов иерархического списка из таблицы
+     * @return array массив элементов иерархического списка
+     */
+    public function getItemsFromTable(): array
+    {
+        return Rubrics::find()->asArray()->all();        
+    }
+    
+    /**
+     * Сохранение элементов иерархического списка в кэш
+     */
+    public function saveItemsToCache()
+    {
+        // преобразуем массив элементов иерархического списка в json
+        $content = json_encode($this->items);
+        // записываем кэш в файл
+        file_put_contents($this->getCachePath(), $content);
     }
 }
